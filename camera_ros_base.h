8d31f85363a61938c23dd705199433883b2476cd
Chao Qu <quchao@seas.upenn.edu>
2014-09-03 14:47:44 -0400

    add a camera nodehandle for supporting stereo camera

--- camera_ros_base.h
+++ camera_ros_base.h
@@ -15,31 +15,30 @@ class CameraRosBase {
   CameraRosBase(const ros::NodeHandle& nh,
                 const std::string& prefix = std::string())
       : nh_{nh},
+        cnh_{nh, prefix},
         it_{nh},
-        camera_publisher_{it_.advertiseCamera("image_raw", 1)},
-        cinfo_manager_{nh},
+        camera_pub_{it_.advertiseCamera(ResolveName(prefix, "image_raw"), 1)},
+        cinfo_mgr_{cnh_},
         fps_{10},
         topic_diagnostic_{
             "image_raw", diagnostic_updater_,
             diagnostic_updater::FrequencyStatusParam(&fps_, &fps_, 0.1, 10),
             diagnostic_updater::TimeStampStatusParam(0, 0.05)} {
     nh_.param<std::string>("frame_id", frame_id_, "camera");
-    nh_.getParam(ResolveParam(prefix, "identifier"), identifier_);
+    nh_.getParam(ResolveName(prefix, "identifier"), identifier_);
     // Setup camera info manager
     std::string camera;
     std::string calib_url;
-    nh_.getParam(ResolveParam(prefix, "camera"), camera);
-    nh_.getParam(ResolveParam(prefix, "calib_url"), calib_url);
-    if (cinfo_manager_.setCameraName(camera) &&
-        cinfo_manager_.validateURL(calib_url) &&
-        cinfo_manager_.loadCameraInfo(calib_url)) {
-      if (!cinfo_manager_.isCalibrated()) {
+    nh_.getParam("camera", camera);
+    nh_.getParam(ResolveName(prefix, "calib_url"), calib_url);
+    if (cinfo_mgr_.setCameraName(camera) && cinfo_mgr_.validateURL(calib_url) &&
+        cinfo_mgr_.loadCameraInfo(calib_url)) {
+      if (!cinfo_mgr_.isCalibrated()) {
         ROS_WARN_STREAM(camera << " not calibarted.");
       }
     }
     image_msg_.reset(new sensor_msgs::Image());
-    cinfo_msg_.reset(
-        new sensor_msgs::CameraInfo(cinfo_manager_.getCameraInfo()));
+    cinfo_msg_.reset(new sensor_msgs::CameraInfo(cinfo_mgr_.getCameraInfo()));
   }
 
   CameraRosBase(const CameraRosBase&) = delete;
@@ -59,7 +58,7 @@ class CameraRosBase {
     cinfo_msg_->header.frame_id = frame_id_;
     image_msg_->header = cinfo_msg_->header;
     if (Grab(image_msg_)) {
-      camera_publisher_.publish(image_msg_, cinfo_msg_);
+      camera_pub_.publish(image_msg_, cinfo_msg_);
       topic_diagnostic_.tick(image_msg_->header.stamp);
     }
     diagnostic_updater_.update();
@@ -68,16 +67,16 @@ class CameraRosBase {
   virtual bool Grab(const sensor_msgs::ImagePtr& image_msg) = 0;
 
  private:
-  std::string ResolveParam(const std::string& prefix,
-                           const std::string& param) {
+  std::string ResolveName(const std::string& prefix, const std::string& param) {
     return nh_.resolveName(prefix.empty() ? param
                                           : ros::names::append(prefix, param));
   }
 
   ros::NodeHandle nh_;
+  ros::NodeHandle cnh_;
   image_transport::ImageTransport it_;
-  image_transport::CameraPublisher camera_publisher_;
-  camera_info_manager::CameraInfoManager cinfo_manager_;
+  image_transport::CameraPublisher camera_pub_;
+  camera_info_manager::CameraInfoManager cinfo_mgr_;
   sensor_msgs::ImagePtr image_msg_;
   sensor_msgs::CameraInfoPtr cinfo_msg_;
   double fps_;
